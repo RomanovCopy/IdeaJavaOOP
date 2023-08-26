9fd1f4299037662210962adeb229124f4d14e010
RomanovCopy <RomanovCopy@gmail.com>
2023-08-26 14:24:13 +0300

    end classwork

---
+++ ClassWorks/src/main/java/com/romanovcopy/Animal.java
@@ -0,0 +1,27 @@
+package com.romanovcopy;
+
+public class Animal {
+
+    String name;
+
+    public Animal(String name) {
+        this.name = name;
+    }
+
+    public Animal() {
+    }
+
+
+    public void animalInfo() {
+        System.out.println("Животное = " + name);
+    }
+
+    public void voice() {
+        System.out.println("Животное = " + name + " издало звук!");
+    }
+
+    public void jump() {
+        System.out.println("Животное = " + name + " подпрыгнуло!");
+    }
+
+}
---
+++ ClassWorks/src/main/java/com/romanovcopy/Cat.java
@@ -0,0 +1,56 @@
+package com.romanovcopy;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cat {
+
+    private String name;
+    private String color;
+    private int age;
+
+    public Cat(String name, String color, int age) {
+        this.name = name;
+        this.color = color;
+//        this.age = age;
+        setAge(age);
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                "name='" + name + '\'' +
+                ", color='" + color + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age < 0) {
+            throw new IllegalArgumentException();
+        } else {
+            this.age = age;
+        }
+    }
+
+}
