fa31c16f35fde67d363b37c283129995287fc191
RomanovCopy <RomanovCopy@gmail.com>
2023-09-08 19:35:56 +0300

    START

--- ClassWorks/src/main/java/com/romanovcopy/studentView/UserService.java
+++
@@ -1,8 +0,0 @@
-package Task2;
-
-import java.util.List;
-
-public interface UserService<T> {
-    List<T> getAll();
-    void create(String name,int score,int age);
-}
--- ClassWorks/src/main/java/com/romanovcopy/studentView/UserView.java
+++
@@ -1,9 +0,0 @@
-package Task2;
-
-import org.example.User;
-
-import java.util.List;
-
-public interface UserView<T extends User> {
-    public void sendOnConsole(List<T> users);
-}
