1c648fb8be522d3ec11be03792c07df2db722406
RomanovCopy <RomanovCopy@gmail.com>
2023-09-13 20:20:52 +0300

    END

--- Homeworks/src/main/java/com/romanovcopy/interfaces/ContactView.java
+++ Homeworks/src/main/java/com/romanovcopy/interfaces/ContactView.java
@@ -5,6 +5,18 @@ import com.romanovcopy.models.Extension;
 
 import java.util.List;
 
+/**
+ * Данный интерфейс принципу единственной ответственности .
+ *        Он определяет три метода, каждый из которых отвечает за конкретную
+ *      ответственность:
+ *            Метод displayContacts отображает список контактов типа T, где T является
+ *            подтипом Person.
+ *            Метод displayExtension отображает информацию о расширении.
+ *            Метод displayErrorMessage отображает сообщение об ошибке.
+ * Каждый метод выполняет только одну задачу, что соответствует принципу единственной ответственности.
+ * Это позволяет легко понять, какие операции выполняет интерфейс и как их использовать.
+ */
+
 public interface ContactView<T extends Person> {
     void displayContacts(List<T> contacts);
     void displayExtension(Extension extension);
--- Homeworks/src/main/java/com/romanovcopy/models/Contact.java
+++ Homeworks/src/main/java/com/romanovcopy/models/Contact.java
@@ -4,6 +4,14 @@ import com.romanovcopy.base.Person;
 
 import java.io.Serializable;
 
+/**
+ * Данный класс Contact удовлетворяет принципу единственной ответственности (SRP).
+ * Он отвечает только за представление контактной информации и несет ответственность
+ * только за хранение имени и номера телефона. Класс не занимается другими функциями,
+ * такими как обработка данных или взаимодействие с базой данных.
+ * Это позволяет классу быть более гибким и легко поддерживаемым.
+ */
+
 public class Contact extends Person implements Serializable {
     public Contact(String name, String phoneNumber) {
         super(name, phoneNumber);
